6de0037121c01d89dbd85c748d305f69ce3904ca
leejh1129 <leejh09876@naver.com>
2024-11-17 23:16:21 +0900

    test

---
+++ src/components/VueComponent.vue
@@ -0,0 +1,25 @@
+<!-- VueComponent.vue -->
+<template>
+    <!-- 주의! 하나의 태그만 등록 -->
+    <div>
+        <label><input type="text" v-model="msg"></label>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+    export default {    // 내보내기
+        name : 'VueComponent',
+        data(){
+            return {
+                msg : '2024-11-15'
+            }
+        }
+    }
+</script>
+
+<style>
+    p {
+        background-color: bisque;
+    }
+</style>
